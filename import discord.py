a36667b4a6f3bf41d6bdf30276dbc5d0add33943
Gyu Tae Bae <50886917+GyuTaeBae0407@users.noreply.github.com>
2022-06-16 22:17:24 -0700

    Add files via upload

---
+++ import discord.py
@@ -0,0 +1,33 @@
+#bot.py파일입니다.
+import discord
+from discord.ext import commands
+from to import Token
+
+bot=commands.Bot(command_prefix='/')
+
+@bot.event
+async def on_ready():
+    print('로그인중입니다. ')
+    print(f"봇={bot.user.name}로 연결중")
+    print('연결이 완료되었습니다.')
+    await bot.change_presence(status=discord.Status.online, activity=None)
+@bot.command(aliases=['hi'])
+async def 안녕(ctx):
+    await ctx.send('안녕하세요.')
+@bot.command(aliases=['hello'])
+async def Hi(ctx):
+    await ctx.send('Hello.')
+@bot.command()
+async def 따라하기(ctx,*,text):
+    await ctx.send(text)
+
+#api call 형식으로해서 (아니면 서버있는걸, 예: lib)
+# command: /Image <-- 커맨드 시작이고
+# /Image (ctx) Computer with a mouse (Text) <-- (ctx, *, text)
+# Image maker <-- (text) input, output --> discord send
+
+bot.run(Token)
+
+
+#-----------------------------
+#to.py파일입니다.
