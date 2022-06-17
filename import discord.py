424e4232087dfe5b47b12098606eea55400363df
Gyu Tae Bae <50886917+GyuTaeBae0407@users.noreply.github.com>
2022-06-16 22:18:36 -0700

    Update import discord.py

--- import discord.py
+++ import discord.py
@@ -1,4 +1,3 @@
-#bot.py파일입니다.
 import discord
 from discord.ext import commands
 from to import Token
@@ -7,9 +6,9 @@ bot=commands.Bot(command_prefix='/')
 
 @bot.event
 async def on_ready():
-    print('로그인중입니다. ')
-    print(f"봇={bot.user.name}로 연결중")
-    print('연결이 완료되었습니다.')
+    print('로그인중. ')
+    print(f"봇={bot.user.name} 연결중")
+    print('연결 완료')
     await bot.change_presence(status=discord.Status.online, activity=None)
 @bot.command(aliases=['hi'])
 async def 안녕(ctx):
@@ -27,7 +26,3 @@ async def 따라하기(ctx,*,text):
 # Image maker <-- (text) input, output --> discord send
 
 bot.run(Token)
-
-
-#-----------------------------
-#to.py파일입니다.
